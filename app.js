1037a01e9a5259222c1ad78762850fec76288e89
David Woolf <davidjohnwoolf@gmail.com>
2017-06-23 21:42:05 -0700

    add get /

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let express = require('express')
 let app = express();
+let path = require('path');
 let port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
-    res.send('Working')
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 app.listen(port, function() {
