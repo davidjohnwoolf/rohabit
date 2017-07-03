b37b3c4d133aa50e4e9eb99ad72fbf9150bb700d
David Woolf <davidjohnwoolf@gmail.com>
2017-07-02 17:39:13 -0700

    update webpack process

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+//import css for webpack
+require('../sass/base.sass');
+
 import Text from './other';
 
 console.log(Text);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,49 @@
-let path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+const extractSass = new ExtractTextPlugin({
+    filename: production ? 'styles.[hash:10].min.css' : 'styles.[hash:10].css'
+});
 
 module.exports = {
     devtool: 'source-map',
     entry: './src/js/main.js',
     module: {
+        rules: [{
+            test: /\.sass$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            })
+        }],
         loaders: [
             {
                 test: /\.js$/,
                 loaders: ['babel-loader'],
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.sass$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: '/node_modules/'
             }
         ]
     },
+    plugins: [
+        extractSass,
+        new HtmlWebpackPlugin({
+            template: 'src/index-template.html'
+        })
+    ],
     output: {
-        path: path.join(__dirname, 'dist/js'),
-        publicPath: '/dist/js',
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: production ? 'bundle.[hash:10].min.js' : 'bundle.[hash:10].js'
     }
 }
