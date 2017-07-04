f64e2ce0490dad844ceb97f333790c373b74202d
David Woolf <davidjohnwoolf@gmail.com>
2017-07-03 23:28:15 -0700

    remove GET / as it already pulls index.html

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ let port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './src', 'index.html'));
-});
-
 app.listen(port, function() {
     console.log('listening on ' + port);
 });
