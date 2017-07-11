6e41ac32ac52090d1688f5ec8fe7519b3c922378
David Woolf <davidjohnwoolf@gmail.com>
2017-07-10 21:47:22 -0700

    set up mongodb and habits route

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let express = require('express')
-let app = express();
-let path = require('path');
-let port = process.env.PORT || 3000;
+const express = require('express')
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+const databaseUrl = 'mongodb://localhost:27017/rohabit';
+
+//set space for database in global scope
+let db;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(port, function() {
-    console.log('listening on ' + port);
+app.get('/habits', (req, res) => {
+	db.collection('habits').find().toArray((err, results) => res.send(results));
+});
+
+MongoClient.connect(databaseUrl, (err, database) => {
+	if (err) return console.log(err);
+
+	db = database;
+
+	app.listen(3000, () => {
+		console.log('listening on ' + port);
+	});
 });
