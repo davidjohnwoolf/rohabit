e450bb55d26dc25d5564a1d3e988239c3e28a9a7
David Woolf <davidjohnwoolf@gmail.com>
2017-07-28 19:49:00 -0700

    update users controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
 	User.find({}, (err, users) => {
 		if (err) return res.json(error);
 
