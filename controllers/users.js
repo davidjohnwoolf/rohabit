55b23c5a401dad45bf9e0c8c74efde3717ea342c
David Woolf <davidjohnwoolf@gmail.com>
2017-07-28 20:19:05 -0700

    update users controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+//index
 router.get('/users', (req, res) => {
 	User.find({}, (err, users) => {
 		if (err) return res.json(error);
 
 		return res.json(users);
+	});
+});
+
+router.post('/user', (req, res) => {
+	const user = new User(req.body);
+
+	user.save((err, user) => {
+		if (err) return res.json(err);
+
+		res.json({ message: 'User Created' });
 	});
 });
 
