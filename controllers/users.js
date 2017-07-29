5ec02f59055884923fe40e95c8680a8f303fc121
David Woolf <davidjohnwoolf@gmail.com>
2017-07-29 13:54:44 -0700

    add body parser

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bodyParser = require('body-parser');
 
-//index
+// body parser middleware
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// index
 router.get('/users', (req, res) => {
 	User.find({}, (err, users) => {
 		if (err) return res.json(error);
@@ -11,7 +16,9 @@ router.get('/users', (req, res) => {
 	});
 });
 
+// create
 router.post('/user', (req, res) => {
+	console.log(res.body)
 	const user = new User(req.body);
 
 	user.save((err, user) => {
